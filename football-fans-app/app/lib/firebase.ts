372e06083b1b3c89a71659563918d049f06ccd64
loic-marigny <156449198+loic-marigny@users.noreply.github.com>
2025-07-13 10:49:11 +0200

    Update firebase.ts

--- football-fans-app/app/lib/firebase.ts
+++ football-fans-app/app/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
   apiKey: 'XXX',
-  authDomain: 'footballconnect-d810b.firebaseapp.com',
-  projectId: 'footballconnect-d810b',
-  storageBucket: 'footballconnect-d810b.appspot.com',
-  messagingSenderId: '260551810739',
-  appId: '1:260551810739:web:7de0bebc39e1040fa45ac6',
-  measurementId: 'G-GV8T8QMPX7',
+  authDomain: 'XXX',
+  projectId: 'XXX',
+  storageBucket: 'XXX',
+  messagingSenderId: 'XXX',
+  appId: 'XXX',
+  measurementId: 'XXX',
 };
 
 // Initialize Firebase
